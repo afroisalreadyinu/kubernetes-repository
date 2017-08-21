63763cadd5adc7be6d1255eeb9b27792e6b8dab4
Ulas Turkmen <ulas.turkmen@twylahelps.com>
2017-08-21 10:50:41 +0200

    Added endpoint to kill process

--- simple-python-app/application.py
+++ simple-python-app/application.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 
 app = Flask('docker-application')
@@ -5,5 +6,9 @@ app = Flask('docker-application')
 @app.route("/")
 def index():
     return 'Hello from the dockerized app'
+
+@app.route("/die")
+def die():
+    os.kill(os.pid())
 
 app.run(host='0.0.0.0', port=8080)
