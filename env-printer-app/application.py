7bc6f370d884d3562455ea8760c75e887d7557b7
Ulas Turkmen <ulas.turkmen@twylahelps.com>
2017-08-25 16:31:36 +0200

    New app to show environment variables

--- env-printer-app/application.py
+++ env-printer-app/application.py
@@ -1,9 +1,11 @@
+import os
+import pprint
 from flask import Flask
 
 app = Flask('docker-application')
 
 @app.route("/")
 def index():
-    return 'Hello from the dockerized app'
+    return pprint.pformat(dict(os.environ))
 
 app.run(host='0.0.0.0', port=8080)
