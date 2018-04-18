b653d827a1d726310f2a824253e5c14b8f1ad1dc
Ulas Turkmen <ulas.turkmen@twylahelps.com>
2018-04-18 14:34:26 +0200

    Fixed return string

--- simple-python-app/application.py
+++ simple-python-app/application.py
@@ -4,6 +4,6 @@ app = Flask('docker-application')
 
 @app.route("/")
 def index():
-    return 'Hello from the dockerized app'
+    return 'Hello from the simple python app\n'
 
 app.run(host='0.0.0.0', port=8080)
