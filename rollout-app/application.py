38e5c94396189549fec226f2c55fc0800f901c15
Ulas Turkmen <ulas.turkmen@twylahelps.com>
2018-04-24 12:59:33 +0200

    Added healthz endpoint, readiness probe

--- rollout-app/application.py
+++ rollout-app/application.py
@@ -1,4 +1,6 @@
 import os
+import time
+
 import pprint
 from flask import Flask
 
@@ -9,4 +11,9 @@ def index():
     host = os.environ['HOSTNAME']
     return "<html><body><h1>{}</h1></body></html>".format(host)
 
+@app.route("/healthz")
+def health():
+    return "OK"
+
+time.sleep(5)
 app.run(host='0.0.0.0', port=8080)
